8b72485c964b8204c1c06d5645e6277aa3f8bcad
Alessandro Tecce <alte5103@colorado.edu>
2016-04-11 15:45:58 -0600

    Compromise.

--- spiders/investigation.py
+++ spiders/investigation.py
@@ -1,5 +1,7 @@
 import scrapy
 
+import multiprocessing
+
 class investigation(scrapy.Spider):
 
 	name = 'investigation'
@@ -11,8 +13,33 @@ class investigation(scrapy.Spider):
 
 	def parse(self, response):
 
-		with open('urls/alphabet_urls.txt', 'w') as f:
+		for suburl in response.xpath("//div[@id='page-letter-search']//@href").re("^/artists/[A-Z0]$"): 
 
-			for suburl in response.xpath("//div[@id='page-letter-search']//@href").re("^/artists/[A-Z0]$"): 
+			self.honor(response.urljoin(suburl))
 
-				f.write(response.urljoin(suburl + '/99999')+'\n')
+	def honor(self, response):
+
+		for suburl in response.xpath("//tr//@href").extract(): 
+
+			self.admire(scrapy.Request(response.urljoin(suburl)))
+
+	def admire(self, response):
+
+		for suburl in response.xpath("//h3[@class='artist-album-label']//@href").extract(): 
+
+			self.experience(scrapy.Request(response.urljoin(suburl)))
+
+	def experience(self, response):
+
+		for suburl in response.xpath("//strong//@href").extract(): 
+
+			self.meditate((scrapy.Request(response.urljoin(suburl))))
+
+	def meditate(self, response):
+
+		for lyrics in response.xpath("//pre[@id='lyric-body-text']/text()").extract():
+
+			for line in lyrics.splitlines():
+
+				print line
+
