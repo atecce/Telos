82e1ebd2cf04f2949e93b1b36ba9553d50cf2081
Alessandro Tecce <alte5103@colorado.edu>
2016-04-12 01:50:50 -0600

    Do, don't be.

--- spiders/investigate.py
+++ spiders/investigate.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+#import time
 import scrapy
 
-def multitask(self, processes, process_target, process_args):
+#def multitask(self, processes, process_target, process_args):
+def multitask(self, process_target, process_args):
 
 	# don't wait for your neighbors
 	import multiprocessing
@@ -12,16 +15,16 @@ def multitask(self, processes, process_target, process_args):
 	process = multiprocessing.Process(target=process_target, args=process_args)
 	process.start()
 
-	# keep track of your progress
-	processes.append(process)
-	alive = [process for process in processes if process.is_alive()]
-
-	# don't overextend
-	while len(alive) >= branching_factor:
-
-	# pace yourself
-	alive = [process for process in processes if process.is_alive()]
-	self.time.sleep(1)
+#	# keep track of your progress
+#	processes.append(process)
+#	alive = [process for process in processes if process.is_alive()]
+#
+#	# don't overextend
+#	while len(alive) >= branching_factor:
+#
+#		# pace yourself
+#		alive = [process for process in processes if process.is_alive()]
+#		time.sleep(1)
 
 class investigation(scrapy.Spider):
 
@@ -34,29 +37,35 @@ class investigation(scrapy.Spider):
 
 	def parse(self, response):
 
-		artists = list()
-
 		for suburl in response.xpath("//div[@id='page-letter-search']//@href").re("^/artists/[A-Z0]$"): 
 
-			self.honor(response.urljoin(suburl))
+			print suburl
+
+			yield scrapy.Request(response.urljoin(suburl), self.honor)
 
 	def honor(self, response):
 
 		for suburl in response.xpath("//tr//@href").extract(): 
 
-			self.admire(scrapy.Request(response.urljoin(suburl)))
+			print '\t', suburl
+
+			yield scrapy.Request(response.urljoin(suburl), self.admire)
 
 	def admire(self, response):
 
 		for suburl in response.xpath("//h3[@class='artist-album-label']//@href").extract(): 
 
-			self.experience(scrapy.Request(response.urljoin(suburl)))
+			print '\t\t', suburl
+
+			yield scrapy.Request(response.urljoin(suburl), self.experience)
 
 	def experience(self, response):
 
 		for suburl in response.xpath("//strong//@href").extract(): 
 
-			self.meditate((scrapy.Request(response.urljoin(suburl))))
+			print '\t\t\t', suburl
+
+			yield scrapy.Request(response.urljoin(suburl), self.meditate)
 
 	def meditate(self, response):
 
@@ -64,4 +73,4 @@ class investigation(scrapy.Spider):
 
 			for line in lyrics.splitlines():
 
-				print line
+				print '\t\t\t\t', line
