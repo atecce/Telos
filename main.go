b2cec94b20b6d22402fe68c7c72c159bfdf623af
atecce <atecce@icloud.com>
2016-11-29 14:57:01 -0500

    First pass at scraping content

--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
-	"strings"
+	"net/url"
 	"sync"
 
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
-)
-
-const (
-	rootURL    = "https://en.wikipedia.org"
-	initialURL = "/w/index.php?title=Special:AllPages&hideredirects=1"
 )
 
 type NavPage struct {
@@ -24,18 +19,36 @@ func (p *NavPage) investigate() {
 
 	// find all pages with content
 	contentPages := scrape.FindAll(p.root, func(n *html.Node) bool {
-		return n.Data == "a"
+		if n.Parent != nil {
+			return n.Parent.Data == "ul" &&
+				scrape.Attr(n.Parent, "class") == "mw-allpages-chunk"
+		}
+		return false
 	})
 
 	// concurrently investigate all the pages
 	for _, page := range contentPages {
 		p.wg.Add(1)
 		go func(page *html.Node) {
+			defer p.wg.Done()
 			if page.FirstChild != nil {
-				_, _ = http.Get(rootURL + scrape.Attr(page, "href"))
-				log.Println("\t", scrape.Attr(page, "href"), page.FirstChild.Data)
+
+				url, err := url.Parse(scrape.Attr(page.FirstChild, "href"))
+				if err != nil {
+					panic(err)
+				}
+
+				root := communicate(url)
+				content := scrape.FindAll(root, func(n *html.Node) bool {
+					return n.Data == "p"
+				})
+
+				for _, node := range content {
+					if node.FirstChild != nil {
+						log.Println("\t", node.FirstChild.Data)
+					}
+				}
 			}
-			p.wg.Done()
 		}(page)
 	}
 
@@ -47,39 +60,66 @@ func (p *NavPage) next() *NavPage {
 
 	// attempt to find element with next page link
 	nextPage, ok := scrape.Find(p.root, func(n *html.Node) bool {
-		if n.FirstChild != nil {
-			return strings.HasPrefix(n.FirstChild.Data, "Next page")
+		if n.Parent != nil {
+			return n.Parent.Data == "div" &&
+				scrape.Attr(n.Parent, "class") == "mw-allpages-nav" &&
+				n == n.Parent.LastChild
 		}
 		return false
 	})
 
 	// return the link if found otherwise return nil
 	if ok {
-		log.Println(scrape.Attr(nextPage, "href"))
-		return newNavPage(scrape.Attr(nextPage, "href"))
+		url, err := url.Parse(scrape.Attr(nextPage, "href"))
+		if err != nil {
+			panic(err)
+		}
+		return newNavPage(url)
 	}
 	return nil
 }
 
-func newNavPage(subURL string) *NavPage {
+func communicate(url *url.URL) *html.Node {
+
+	// fix sub urls
+	if url.Scheme == "" {
+		url.Scheme = "https"
+	}
+	if url.Host == "" {
+		url.Host = "en.wikipedia.org"
+	}
+	log.Println(url)
 
 	// make request
-	resp, err := http.Get(rootURL + subURL)
+	resp, err := http.Get(url.String())
+	defer resp.Body.Close()
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
+
+	// return the root of DOM
+	root, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return root
+}
+
+func newNavPage(url *url.URL) *NavPage {
 
 	// construct new page
 	p := new(NavPage)
-	p.root, err = html.Parse(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	p.root = communicate(url)
 	return p
 }
 
 func main() {
+
+	// initialize url
+	initialURL, err := url.Parse("https://en.wikipedia.org/w/index.php?title=Special:AllPages")
+	if err != nil {
+		panic(err)
+	}
 
 	// singly linked list
 	for p := newNavPage(initialURL); p != nil; p = p.next() {
