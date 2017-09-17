317f59ab92fe0d95bfb8c57b45b91e234bd12cc9
atec <root@atec.pub>
2017-09-17 13:08:33 -0400

    begin a proper model

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -13,6 +13,23 @@ type Canvas struct {
 	name string
 }
 
+type Artist struct {
+	name string
+}
+
+type Album struct {
+	artist *Artist
+
+	name string
+}
+
+type Song struct {
+	album *Album
+
+	name   string
+	lyrics string
+}
+
 func New(name string) *Canvas {
 
 	db, err := sql.Open("sqlite3", name+".db")
@@ -25,20 +42,22 @@ func New(name string) *Canvas {
 
 	_, err = db.Exec(`create table if not exists albums (
 
-				     title       text not null,
 				     artist	 text not null,
 
-				     primary key (title, artist),
+				     name        text not null,
+
+				     primary key (name, artist),
 				     foreign key (artist) references artists (name))`)
 
 	_, err = db.Exec(`create table if not exists songs (
 
-				     title       text not null,
 				     album 	 text not null,
+
+				     name        text not null,
 				     lyrics      text,
 
-				     primary key (album, title),
-				     foreign key (album) references albums (title))`)
+				     primary key (album, name),
+				     foreign key (album) references albums (name))`)
 
 	if err != nil {
 		log.Println("Failed to create tables:", err)
@@ -50,33 +69,33 @@ func New(name string) *Canvas {
 	}
 }
 
-func (canvas *Canvas) AddArtist(artist_name string) {
+func (canvas *Canvas) AddArtist(name string) {
 
 	tx, err := canvas.db.Begin()
 	stmt, err := tx.Prepare("insert or replace into artists (name) values (?)")
 	defer stmt.Close()
-	_, err = stmt.Exec(artist_name)
+	_, err = stmt.Exec(name)
 	tx.Commit()
 
 	if err != nil {
-		log.Println("Failed to add artist", artist_name+":", err)
+		log.Println("Failed to add artist", name+":", err)
 	}
 }
 
-func (canvas *Canvas) AddAlbum(artist_name, album_title string) {
+func (canvas *Canvas) AddAlbum(artist, name string) {
 
 	tx, err := canvas.db.Begin()
-	stmt, err := tx.Prepare("insert or replace into albums (artist, title) values (?, ?)")
+	stmt, err := tx.Prepare("insert or replace into albums (artist, name) values (?, ?)")
 	defer stmt.Close()
-	_, err = stmt.Exec(artist_name, album_title)
+	_, err = stmt.Exec(artist, name)
 	tx.Commit()
 
 	if err != nil {
-		log.Fatal("Failed to add album", album_title, "by", artist_name+":", err)
+		log.Fatal("Failed to add album", name, "by", artist+":", err)
 	}
 }
 
-func (canvas *Canvas) AddSong(album_title, song_title, lyrics string) {
+func (canvas *Canvas) AddSong(album, name, lyrics string) {
 
 	tx, err := canvas.db.Begin()
 	if err != nil {
@@ -86,26 +105,26 @@ func (canvas *Canvas) AddSong(album_title, song_title, lyrics string) {
 	var failed bool
 	for {
 
-		stmt, err := tx.Prepare("insert or replace into songs (album, title, lyrics) values (?, ?, ?)")
+		stmt, err := tx.Prepare("insert or replace into songs (album, name, lyrics) values (?, ?, ?)")
 		if err != nil {
 			failed = true
-			log.Println("Error in .Prepare: Failed to add song", song_title, "in album", album_title+":", err)
+			log.Println("Error in .Prepare: Failed to add song", name, "in album", album+":", err)
 			time.Sleep(time.Second)
 			continue
 		}
 		defer stmt.Close()
 
-		_, err = stmt.Exec(album_title, song_title, lyrics)
+		_, err = stmt.Exec(album, name, lyrics)
 		if err != nil {
 			failed = true
-			log.Println("Error in .Exec: Failed to add song", song_title, "in album", album_title+":", err)
+			log.Println("Error in .Exec: Failed to add song", name, "in album", album+":", err)
 			time.Sleep(time.Second)
 			continue
 		}
 		tx.Commit()
 
 		if failed {
-			log.Println("Successfully added song", song_title, "in album", album_title)
+			log.Println("Successfully added song", name, "in album", album)
 		}
 		return
 	}
