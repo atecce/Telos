0e123784f278c44880587f9266d5f2c0bda63779
atec <root@atec.pub>
2018-09-03 18:37:11 -0400

    append commits

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/pachyderm/pachyderm/src/client"
 )
 
+const (
+	repoName   = "investigations"
+	branchName = "master"
+)
+
 func main() {
 
 	pachyderm, err := client.NewOnUserMachine(true, "")
@@ -13,20 +18,31 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := pachyderm.CreateRepo("investigations"); err != nil {
+	if err := pachyderm.CreateRepo(repoName); err != nil {
 		log.Println("creating investigations:", err)
 	}
 
-	if err := pachyderm.CreateBranch("investigations", "master", "", nil); err != nil {
-		log.Println("creating master branch:", err)
-	}
-
-	commit, err := pachyderm.StartCommit("investigations", "master")
+	commit, err := pachyderm.StartCommit(repoName, branchName)
 	if err != nil {
 		log.Println("starting commit:", err)
 	}
 
-	if err := pachyderm.PutFileURL("investigations", commit.GetID(), "root", "http://www.lyrics.net", false, true); err != nil {
-		log.Println("putting file:", err)
+	if err := pachyderm.CreateBranch(repoName, branchName, commit.GetID(), nil); err != nil {
+		log.Println("creating master branch:", err)
+	}
+
+	branch, err := pachyderm.InspectBranch(repoName, branchName)
+	if err != nil {
+		log.Println("inspecting branch:", err)
+	}
+
+	head := branch.Head.GetID()
+	if err := pachyderm.PutFileURL(repoName, head, "root", "http://www.lyrics.net", false, true); err != nil {
+		log.Println("putting with head:", head)
+		log.Println("err:", err)
+	}
+
+	if err := pachyderm.FinishCommit(repoName, commit.GetID()); err != nil {
+		log.Println("finishing commit:", err)
 	}
 }
