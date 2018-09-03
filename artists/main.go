42feabf97d8f16c6d7673ec45f72a9672dfeab6b
atec <root@atec.pub>
2018-09-03 19:59:43 -0400

    add regex to extract only artist links

--- artists/main.go
+++ artists/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
@@ -30,14 +31,22 @@ func main() {
 		}
 
 		for _, link := range scrape.FindAll(root, func(n *html.Node) bool {
+
+			isArtist, err := regexp.MatchString("^artist/*", scrape.Attr(n, "href"))
+			if err != nil {
+				log.Println("matching artist link", err)
+				return false
+			}
+
 			if n.Parent != nil {
-				return n.Parent.Data == "strong" && n.Data == "a"
+				return n.Parent.Data == "strong" && n.Data == "a" && isArtist
 			}
 			return false
 		}) {
 			if link.FirstChild != nil {
 				log.Println(scrape.Attr(link, "href"))
 				log.Println(link.FirstChild.Data)
+				println()
 			}
 		}
 
