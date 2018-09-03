d63db8fc00c5662439f76c4ed17340d45825b246
atec <root@atec.pub>
2018-09-03 18:02:49 -0400

    and once again we'll be reborn

--- canvas/resource.go
+++
@@ -1,5 +0,0 @@
-package canvas
-
-type resource interface {
-	Put()
-}
--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"github.com/de-nova-stella/investigations/www"
+	"log"
+
+	"github.com/pachyderm/pachyderm/src/client"
 )
 
 func main() {
 
-	wittgenstein := www.New()
-	wittgenstein.Run()
+	pachyderm, err := client.NewOnUserMachine(true, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := pachyderm.CreateRepo("investigations"); err != nil {
+		log.Println("creating investigations:", err)
+	}
+
+	if err := pachyderm.CreateBranch("investigations", "master", "", nil); err != nil {
+		log.Println("creating master branch:", err)
+	}
+
+	commit, err := pachyderm.StartCommit("investigations", "master")
+	if err != nil {
+		log.Println("starting commit:", err)
+	}
+
+	if err := pachyderm.PutFileURL("investigations", commit.GetID(), "root", "http://www.lyrics.net", false, true); err != nil {
+		log.Println("putting file:", err)
+	}
 }
