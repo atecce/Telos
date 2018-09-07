94fa1e56b66fdafcf118aad15bc9b4482ee7f574
atec <root@atec.pub>
2018-09-07 15:02:04 -0400

    fire and forget

--- letters/main.go
+++ letters/main.go
@@ -14,26 +14,9 @@ const (
 	branchName = "master"
 )
 
-type job struct {
-	client *client.APIClient
-	path   string
-
-	head string
-	char string
-	url  string
-}
-
-func (j job) run() error {
-
-	log.Println("putting file at path", j.path)
-	if err := j.client.PutFileURL(repoName, j.head, j.char, j.url, false, true); err != nil {
-		log.Println("putting with head:", j.head)
-		return err
-	}
-	return nil
-}
-
 func main() {
+
+	var wg sync.WaitGroup
 
 	pachyderm, err := client.NewOnUserMachine(true, "")
 	if err != nil {
@@ -43,18 +26,6 @@ func main() {
 	if err := pachyderm.CreateRepo(repoName); err != nil {
 		log.Println("creating repo:", err)
 	}
-
-	var wg sync.WaitGroup
-	pool := make(chan job, 100)
-	go func() {
-		for {
-			job := <-pool
-			if err := job.run(); err != nil {
-				log.Println("error running job", err)
-			}
-			wg.Done()
-		}
-	}()
 
 	commit, err := pachyderm.StartCommit(repoName, branchName)
 	if err != nil {
@@ -67,19 +38,21 @@ func main() {
 
 	for _, c := range "0ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
 
-		char := string(c)
-		u.Path = path.Join("artists", char, "99999")
+		fileName := string(c)
+		u.Path = path.Join("artists", fileName, "99999")
 		url := u.String()
-		pool <- job{
-			client: pachyderm,
-			path:   u.Path,
-			head:   head,
-			char:   char,
-			url:    url,
-		}
+
+		go func(fileName, path, url string) {
+			defer wg.Done()
+
+			log.Println("putting file at path", path)
+			if err := pachyderm.PutFileURL(repoName, head, fileName, url, false, true); err != nil {
+				log.Println("putting with head:", head)
+			}
+		}(fileName, u.Path, url)
+
 		wg.Add(1)
 	}
-
 	wg.Wait()
 
 	if err := pachyderm.FinishCommit(repoName, commit.GetID()); err != nil {
